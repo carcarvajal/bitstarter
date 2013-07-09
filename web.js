65d742d785d8271ee9852e0be6950da23027e20b
Carlos Velez <carcarvajal@gmail.co>
2013-07-09 21:53:41 +0000

    Updated web.js2

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 3!');
   fs.readFileSync("index.html");
 });
 
