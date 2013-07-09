a3a53beea11d5267656638cca2d7dba97e4155ec
Carlos Velez <carcarvajal@gmail.co>
2013-07-09 23:21:09 +0000

    Updated web.js3

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var app = express.createServer(express.logger());
 var buffer=new Buffer(200);
 app.get('/', function(request, response) {
   response.send('Hello World 5!');
-  fs.readFileSync('index.html','utf-8').toString();
 });
+
+fs.readFileSync('index.html','utf-8').toString();
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
